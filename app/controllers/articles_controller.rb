7604166755ea068573f97d93dd610ba012d53b2d
Thein Naing <theinnaing0908@gmail.com>
2023-03-07 17:12:12 +0800

    controller and views are implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,43 @@
 class ArticlesController < ApplicationController
-  class TasksController < ApplicationController
-    before_action :set_task, only: %i[show edit update destroy]
-    def index
-      @articles = Article.all
-    end
-
-    def show
-      @article
-    end
-
-    def new
-      @article = Article.new
-    end
-
-    def create
-      Article.create!(params_article)
-      redirect_to article_path([:id])
-    end
-
-    def edit
-      @article
-    end
-
-    def update
-      @article.update!(params_article)
-      redirect_to article_path(@article)
-    end
-
-    def destroy
-      @article.destroy
-      redirect_to articless_path
-    end
-
-    private
-
-    def params_task
-      params.require(:article).permit(:title, :details)
-    end
-
-    def set_task
-      @article = Article.find(params[:id])
-    end
+  before_action :set_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
   end
+
+  def show
+    @article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create!(params_article)
+    redirect_to article_path([:id])
+  end
+
+  def edit
+    @article
+  end
+
+  def update
+    @article.update!(params_article)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'new/show'
-  get 'new/create'
-  get 'new/edit'
-  get 'new/update'
-  get 'new/destroy'
+  # get 'new/show'
+  # get 'new/create'
+  # get 'new/edit'
+  # get 'new/update'
+  # get 'new/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
